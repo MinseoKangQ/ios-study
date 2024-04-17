0c01f865f294b26f30accad3775eb190a06b0c7c
MinseoKangQ <kms02171@naver.com>
2024-04-17 16:56:56 +0900

    :tada: 5주차 실습 시작

---
+++ practice-ch05/practice-ch05/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  practice-ch05
+//
+//  Created by 강민서 on 4/17/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
